b4803607a50d11e277805f2ac79de4a7290c7a24
chatdanai <chatdanai.changphan@gmail.com>
2022-03-22 16:57:41 +0700

    template example

--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@ import "./styles/main.scss"
 
 import CookieConsent from "./models/CookieConsent"
 
-if (typeof exports !== 'undefined') {
-  module.exports = CookieConsent
-} else {
-  window.CookieConsent = CookieConsent
-}
+/*
+//template example
+//replace value upto user setting and then generate js file for each user
+
+const cc = new CookieConsent(
+  {
+    "palette": {
+      "popup": {
+        "background": "#000"
+      },
+      "button": {
+        "background": "#f1d600"
+      }
+    },
+    "theme": "classic"
+  }
+);
+*/
