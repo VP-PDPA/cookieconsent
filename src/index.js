b98185c199b7b8e8865f8abdd9f7be36ba1a7021
chatdanai <chatdanai.changphan@gmail.com>
2022-05-29 15:36:15 +0700

    example

--- src/index.js
+++ src/index.js
@@ -1,24 +1,116 @@
 "use strict"
+/*
+console.log('start global');
+export const dataScanner =  [
+  {
+    url: 'https://www.googletagmanager.com/gtag/js?id=G-BYM1FJKL9N&l=dataLayer&cx=c',
+    domain: 'googletagmanager.com',
+    categories: 'marketing',
+    node: 'script'
+  },
+  {
+    url: 'https://www.google-analytics.com/analytics.js',
+    domain: 'google-analytics.com',
+    categories: 'analytics',
+    node: 'script'
+  },
+  {
+    url: 'https://www.googletagmanager.com/gtag/js?id=UA-226567081-1',
+    domain: 'googletagmanager.com',
+    categories: 'marketing',
+    node: 'script'
+  },
+  {
+    url: 'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FPrimesSolution%2F&tabs&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=2402138786748434',
+    domain: 'facebook.com',
+    categories: 'marketing',
+    node: 'script'
+  }
+];
+//init block
+const observer = new MutationObserver(mutations => {
+  mutations.forEach(({ addedNodes }) => {
+      addedNodes.forEach(node => {
+          if (node.tagName === 'SCRIPT') {
+              for(let i=0; i<dataScanner.length; i++) {
+                  let record = dataScanner[i];
+                  if (record.node !== 'script')
+                      continue;
+                  //if (currentConsentStatus[record.categories.toUpperCase()] !== 'DENY')
+                  //    continue;
+                  if (node.src == dataScanner[i].url) {
+                      node.setAttribute('categories', record.categories);
+                      let data = node.src;
+                      node.removeAttribute('src');
+                      node.setAttribute('data-src',data);
+                  }
+                  
+              }
+          }
+      });
+  });
+});
+
+//export default observer;
+console.log('init');
+observer.observe(window.document.documentElement, {
+  childList: true,
+  subtree: true
+});
 
 import "./styles/main.scss"
 
-import CookieConsent from "./models/CookieConsent"
+import CPopup from "./models/Popup"
 
-/*
-//template example
-//replace value upto user setting and then generate js file for each user
+//block/unblock
+const updateBlockUnblock = (category, status) => {
+  console.log(category + ' ' + status);
+  let script = document.getElementsByTagName('script');
 
-const cc = new CookieConsent(
-  {
-    "palette": {
-      "popup": {
-        "background": "#000"
-      },
-      "button": {
-        "background": "#f1d600"
+  //script
+  for (let i = 0; i < script.length; i++) {
+      for(let j=0; j<dataScanner.length; j++) {
+        let record = dataScanner[j];
+        let node = script[i];
+        console.log(node.getAttribute('data-src'));
+        
+        if (record.node !== 'script' || (node.src !== record.url && node.getAttribute('data-src') !== record.url)
+          || record.categories.toUpperCase() !== category)
+            continue;
+        console.log('debug 1');
+        if (status !== 'ALLOW') {
+          if (node.getAttribute('data-src')) {
+            node.setAttribute('src', node.getAttribute('data-src'));
+            node.removeAttribute('data-src');
+          }
+        }
+        else {
+            node.setAttribute('categories', record.categories);
+            let data = node.src;
+            node.removeAttribute('src');
+            node.setAttribute('data-src',data);
+        }      
       }
-    },
-    "theme": "classic"
   }
-);
+};
+
+const showDialog = () => {
+  const param = JSON.parse('{"palette":{"popup":{"background":"#FF3937A3","text":"#FFFFFFFF"},"button":{"background":"#FFE62576","text":"#FFFFFFFF"}},"position":"bottom-left","type":"opt-in-detail","layout":"detail","content":{"href":"https://www.example.com/yourpolicy3.html"}}');
+  const currentCC = new CPopup(param);
+  currentCC.setStatuses();
+  if (currentCC.hasAnswered() !== true) {
+      currentCC.open();
+  }
+  else {
+  
+  } 
+  
+  currentCC.on( 'statusChanged', (p, a) => {
+    let part = p.split('_');
+    updateBlockUnblock(part[part.length-1], a)
+  });
+  
+  let currentConsentStatus = currentCC.exportCurrentStatuses();
+  console.log(currentConsentStatus);
+}
 */
